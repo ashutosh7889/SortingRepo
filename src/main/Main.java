9096f51e1db972e70a140c6e12d2e77b2374c331
ashu <ashutosh.kuma_cs18>
2020-04-18 23:27:39 +0530

    add menu item for Quick sort.

--- src/main/Main.java
+++ src/main/Main.java
@@ -29,6 +29,7 @@ public class Main {
         System.out.println("Press 1 to sort this array using Bubble Sort");
         System.out.println("Press 2 to sort this array using Selection Sort");
         System.out.println("Press 3 to sort this array using Insertion Sort");
+        System.out.println("Press 4 to sort this array using Quick Sort");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1: /*BUBBLE SORT*/
