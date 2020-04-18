20f9d3f299cb105e9669fd638420da22a7f7d076
ashu <ashutosh.kuma_cs18>
2020-04-18 23:40:07 +0530

    add swap method inside QuickSort Class.

--- src/quicksort/QuickSort.java
+++ src/quicksort/QuickSort.java
@@ -8,6 +8,16 @@ public class QuickSort {
     }
 
     private static int partition(int array[], int leftIndex, int rightIndex) {
+        int pivot = array[rightIndex];
 
+        int partitionIndex = leftIndex;
+        for (int i = leftIndex; i < rightIndex; i++) {
+            if (array[i] <= pivot) {
+
+            }
+        }
+    }
+
+    private static void swap(int[] array, int i, int j) {
     }
 }
