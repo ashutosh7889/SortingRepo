692272c61d823c9d8f1422731ab4225a1dee3fba
ashu <ashutosh.kuma_cs18>
2020-04-18 23:36:03 +0530

    add partition metgod inside the QuickSort Class.

--- src/quicksort/QuickSort.java
+++ src/quicksort/QuickSort.java
@@ -2,6 +2,12 @@ package quicksort;
 
 public class QuickSort {
     public static void sort(int array[], int leftIndex, int rightIndex) {
+        if (leftIndex < rightIndex) {
+            int pivot = partition
+        }
+    }
+
+    private static int partition(int array[], int leftIndex, int rightIndex) {
 
     }
 }
