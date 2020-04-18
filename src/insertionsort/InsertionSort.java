38870e67ad8117f942f26971fc88db82e06bd2cf
ashu <ashutosh.kuma_cs18>
2020-04-18 23:16:30 +0530

    call the insert method inside the sort method of InsertionSort Class.

--- src/insertionsort/InsertionSort.java
+++ src/insertionsort/InsertionSort.java
@@ -3,12 +3,16 @@ package insertionsort;
 public class InsertionSort {
     public static void sort(int[] array) {
         for (int i = 1; i < array.length; i++) {
-
+            insert(array, i);
         }
     }
 
     private static void insert(int[] array, int nextIndex) {
         int nextValue = array[nextIndex];
-
+        while (nextIndex > 0 && nextValue < array[nextIndex - 1]) {
+            array[nextIndex] = array[nextIndex - 1];
+            nextIndex--;
+        }
+        array[nextIndex] = nextValue;
     }
 }
