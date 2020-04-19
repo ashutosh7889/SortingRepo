2611f795cf11bb6010ba6dc94da09fde123c284a
ashu <ashutosh.kuma_cs18>
2020-04-19 14:52:33 +0530

    add menu item for the merge sort.

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,6 +31,7 @@ public class Main {
         System.out.println("Press 2 to sort this array using Selection Sort");
         System.out.println("Press 3 to sort this array using Insertion Sort");
         System.out.println("Press 4 to sort this array using Quick Sort");
+        System.out.println("Press 5 to sort this array using Merge Sort");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1: /*BUBBLE SORT*/
