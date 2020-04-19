df610aec9166cbefa0a592820292363c5b7f1931
ashu <ashutosh.kuma_cs18>
2020-04-19 15:00:23 +0530

    create merge method inside the MergeSort Class.

--- src/mergesort/MergeSort.java
+++ src/mergesort/MergeSort.java
@@ -2,5 +2,20 @@ package mergesort;
 
 public class MergeSort {
     public static void sort(int array[]) {
+        if (array.length > 1) {
+            int halfSize = array.length / 2;
+
+            int[] leftArray = new int[halfSize];
+            int[] rightArray = new int[array.length - halfSize];
+
+            System.arraycopy(array, 0, leftArray, 0, halfSize);
+            System.arraycopy(array, halfSize, rightArray, 0, array.length - halfSize);
+
+            sort(leftArray);
+            sort(rightArray);
+        }
+    }
+
+    public static void merge(int[] outputArray, int[] leftSubArray, int[] rightSubArray) {
     }
 }
